b5db5ab61e34e7492d370c30c4a45e7991fd22b9
lokesh9976 <lokeshkrishnamoorthy2002@gmail.com>
2026-07-04 20:52:05 +0530

    railway

--- print-bridge/src/config.ts
+++ print-bridge/src/config.ts
@@ -95,7 +95,7 @@ const defaultConfig: BridgeConfig = {
     },
     {
       name: 'QR POS',
-      url: 'https://new-qrcode-production-fe4e.up.railway.app',
+      url: 'https://qrcode-conestone-production.up.railway.app',
       enabled: true
     }
   ]
